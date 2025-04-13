7780d01bc95da3cc9402fe0b2f613824e7010f9a
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-04-13 14:02:32 -0300

    feat: create route to create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(@Body dto: CreateUserDto): void {}
+}
